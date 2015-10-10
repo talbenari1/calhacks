736a1ee817d3c95d2de32cc037bd8efaa927134e
Tal Ben-Ari <talbenari1@gmail.com>
2015-10-09 23:48:41 -0700

    Got ID creation working properly, installed socket.io

--- index.js
+++ index.js
@@ -2,17 +2,28 @@
 
 let config = require('./config.json'); // our configuration object
 let app = require('express')(); // the application itself
-let crypto = require('crypto'); // for random id generation
+let crypto = require('crypto');
 let rooms = new Map();
 
 app.get('/', (req, res) => {
   // Create a unique ID for the user
   let id;
   do {
-    id = crypto.randomBytes(6);
+    id = crypto.randomBytes(6).toString('base64');
   } while (rooms.has(id));
 
-  rooms.set(id, []);
+  rooms.set(id, true);
+
+  res.send(id);
+});
+
+app.get('/:id', (req, res) => {
+  let id = req.params.id;
+  if (rooms.has(id)) {
+    res.send(id);
+  } else {
+    res.send('Oh no! That room does not exist');
+  }
 });
 
 let server = app.listen(config.port, () => {
