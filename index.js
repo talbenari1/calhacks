38d7f39a08b90a33fac37a367b8592d0fa02f7e5
Tal Ben-Ari <talbenari1@gmail.com>
2015-10-10 02:15:59 -0700

    Worked on server-side module data loading

--- index.js
+++ index.js
@@ -10,16 +10,39 @@ let rooms = new Map();
 
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', (req, res) => {
-  // Create a unique ID for the user
-  let id;
-  do {
-    id = crypto.randomBytes(6).toString('base64');
-  } while (rooms.has(id));
+io.on('connection', socket => {
+  console.log('A user connected');
 
-  rooms.set(id, true);
+  socket.on('request-new-room', () => {
+    // Create a unique ID for the user
+    let id;
+    do {
+      id = crypto.randomBytes(6).toString('base64');
+    } while (rooms.has(id));
 
-  res.send(id);
+    // Create a namespace for the workspace
+    let nsp = io.of(id);
+
+    nsp.on('connection', socket => {
+      let flags = rooms.get(id).flags;
+      // Adding a new module
+      socket.on('join-channel', channel => {
+        if (!flags[channel]) {
+          socket.to(channel).emit('request-data');
+          flags[channel] = true;
+        }
+      });
+      socket.on('respond-data', data => {
+        if (flags[channel]) {
+          socket.to(channel).emit('respond-data', data);
+          flags[channel] = false;
+        }
+      });
+    });
+
+    rooms.set(id, {nsp, flags: {}});
+    socket.emit('respond-new-room', id);
+  });
 });
 
 app.get('/:id', (req, res) => {
@@ -29,7 +52,7 @@ app.get('/:id', (req, res) => {
     res.send(`Connected to room ${id}`);
   } else {
     // Room doesn't exist
-    res.send(`Room ${id} does not exist`);
+    res.sendFile('public/404.html');
   }
 });
 
