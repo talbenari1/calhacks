f9fd73ac1bb13223d2449050172d5cde54abf14f
Tal Ben-Ari <talbenari1@gmail.com>
2015-10-09 23:21:09 -0700

    Began work on unique ID creator

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 'use strict';
 
-let config = require('./config.json');
-let app = require('express')();
+let config = require('./config.json'); // our configuration object
+let app = require('express')(); // the application itself
+let crypto = require('crypto'); // for random id generation
+let rooms = new Map();
 
 app.get('/', (req, res) => {
-  res.send('Hello, world!');
+  // Create a unique ID for the user
+  let id;
+  do {
+    id = crypto.randomBytes(6);
+  } while (rooms.has(id));
+
+  rooms.set(id, []);
 });
 
 let server = app.listen(config.port, () => {
